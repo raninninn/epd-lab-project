bbc6507e64556fcb5c38f95e058e08091d48e456
raninninn <matteshilli@gmail.com>
2026-07-05 23:30:14 +0200

    add argument parsing to python network

--- weight_export/network.py
+++ weight_export/network.py
@@ -14,15 +14,32 @@ from brevitas.quant.scaled_int import (
     Int8WeightPerTensorFloat
 )
 from brevitas.core.restrict_val import RestrictValueType
+import argparse
+
+##########
+# IMPORTANT: If you want to use a different quantization bit width, you have to
+# change the quantization strategies (Int8WeightPerTensorPoT and so on), as
+# well as the bit width bw!
+###########
+
+parser = argparse.ArgumentParser(description="Train a quantised LeNet1-based CNN on the MNIST dataset and export its weights in weights_c1.h, weights_c3.h and weights_out.h.")
+parser.add_argument("-b", "--bsize", type=int, default=64, help="Batch size")
+parser.add_argument("-e", "--epochs", type=int, default=1, help="Epochs to train")
+parser.add_argument("-s", "--seed", type=int, default=42, help="Set seed manually")
+parser.add_argument("-c", "--report-scales", action='store_true', help="Print the activation scales")
+parser.add_argument("-p", "--report-predictions", action='store_true', help="Print the predictions")
+parser.add_argument("-a", "--report-accuracy", action='store_true', help="Print the test accuracy")
+parser.add_argument("-q", "--suppress-progress", action='store_false', help="Suppress progress reporting")
+args = parser.parse_args()
 
 class Int8WeightPerTensorPoT(Int8WeightPerTensorFloat):
     restrict_scaling_type = RestrictValueType.POWER_OF_TWO
 class Int8ActPerTensorPoT(Int8ActPerTensorFloat):
     restrict_scaling_type = RestrictValueType.POWER_OF_TWO
 
-bsize = 64
-num_epochs = 1
-seed = 42
+bsize = args.bsize
+num_epochs = args.epochs
+seed = args.seed
 bw = 8
 
 transform = transforms.Compose([
@@ -65,16 +82,12 @@ model = nn.Sequential(
 model.train()
 
 torch.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-# Check if GPU is available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
 model.to(device)
-
-# Training loop
-train_losses = []
 
 for epoch in range(num_epochs):
     running_loss = 0.0
@@ -90,13 +103,47 @@ for epoch in range(num_epochs):
 
         running_loss += loss.item()
         if (i+1) % 100 == 0:
-            train_losses.append(running_loss/100)
             running_loss = 0.0
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/100:.4f}')
+    if args.suppress_progress:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/100:.4f}')
 
 model.eval()
 correct = 0
 total = 0
+
+with torch.no_grad():
+    capture_scales = args.report_scales
+    output_predictions = args.report_predictions
+    scales = []
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        x = model[0](images)
+        x = model[1](x)
+        x = model[2](x)
+        if capture_scales:
+            scales.append(x.scale)
+        x = model[3](x)
+        x = model[4](x)
+        x = model[5](x)
+        if capture_scales:
+            scales.append(x.scale)
+        x = model[6](x)
+        x = model[7](x)
+        x = model[8](x)
+        outputs = model[9](x)
+        if capture_scales:
+            scales.append(outputs.scale)
+            print(scales)
+            capture_scales = False
+        _, predicted = torch.max(outputs.value, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        if output_predictions:
+            print(predicted.data.item())
+
+if args.report_accuracy:
+    accuracy = 100 * correct / total
+    print(f'Accuracy on the test set: {accuracy:.2f}%')
 
 def numpy_to_c_array(arr, var_name="my_array", c_type="param_t"):
     shape = 1
@@ -106,55 +153,15 @@ def numpy_to_c_array(arr, var_name="my_array", c_type="param_t"):
     body = body.replace('[', '{').replace(']', '}')
     return f"const {c_type} {var_name.upper()}[{shape}] = {body};"
 
-def export_tensor(qtensor, name):
-    arr = qtensor.int().cpu().numpy().flatten()
-
-    with open(f"{name}.h", "w") as f:
-        f.write("#include <stdint.h>\n\n")
-        f.write(numpy_to_c_array(arr, name, c_type="int32_t"))
-        f.write(f"\n\n#define {name.upper()}_SIZE {arr.size}\n")
-
-with torch.no_grad():
-    y = True
-    for images, labels in test_loader:
-        images, labels = images.to(device), labels.to(device)
-        x0 = model[0](images)
-        x = model[1](x0)
-        acc = F.conv2d(x0.int().float(), model[1].quant_weight().int().float(), bias=None)
-        if y:
-            export_tensor(acc, "c1_out")
-            y = False
-        x = model[2](x)
-        #print(x.scale)
-        x = model[3](x)
-        x = model[4](x)
-        x = model[5](x)
-        #print(x.scale)
-        x = model[6](x)
-        x = model[7](x)
-        x = model[8](x)
-        outputs = model[9](x)
-        #print(outputs.scale)
-        _, predicted = torch.max(outputs.value, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        print(predicted.data.item())
-
-accuracy = 100 * correct / total
-print(f'Accuracy on the test set: {accuracy:.2f}%')
-
 def export_weights(layer_index, layer_name):
     fname = f"{layer_name}.h"
-    header = "#include <stdint.h>\n\n"
     tensor = model[layer_index].quant_weight()
     with open(fname, "w") as f:
-        f.write(header)
         f.write(
                 numpy_to_c_array(tensor.int().cpu().numpy().flatten(),
                 layer_name))
-        f.write(f"\nparam_t {layer_name.upper()}_SCALE = {(-torch.log2(tensor.scale)).int()};")
+        f.write(f"param_t {layer_name.upper()}_SCALE = {(-torch.log2(tensor.scale)).int()};")
 
-# Export weights
 export_weights(1, "weights_c1")
 export_weights(4, "weights_c3")
 export_weights(8, "weights_out")
